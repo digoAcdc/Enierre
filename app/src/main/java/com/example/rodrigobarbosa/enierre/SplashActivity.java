95b908fc59ea5b70c079f5d6b2fd58d1154ac864
rodrigo silveira <rodrigo.silveira@b2wdigital.com>
2017-07-19 18:18:17 -0300

    inicial

--- app/src/main/java/com/example/rodrigobarbosa/enierre/SplashActivity.java
+++ app/src/main/java/com/example/rodrigobarbosa/enierre/SplashActivity.java
@@ -2,12 +2,19 @@ package com.example.rodrigobarbosa.enierre;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://www.enierremarcenaria.com.br");
     }
 }
